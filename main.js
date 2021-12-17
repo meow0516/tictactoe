ccf071a64008dba3ca741997183a608cb6d89275
AzureAD\JaneChan <meow0516@gmail.com>
2021-12-17 11:31:17 +0800

    main.js: use querySelectorAll to choose tictactoe box function: check current player, check if number been used, display O,X on choosed box

--- main.js
+++ main.js
@@ -3,3 +3,53 @@ import './main.scss'
 document.querySelector('#app').innerHTML = `
   <h1>This is a Tic Tac Toe!</h1>
 `
+let click = 0;
+let usedNumber = [];
+let players = {
+  playerOdd: [],
+  playerEven: [],
+}
+let winnerArray = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9],
+  [1,4,7],
+  [2,5,8],
+  [3,6,9],
+  [1,5,9],
+  [3,6,7]
+]
+  
+
+
+document.querySelector('.submit').addEventListener('click',function(e){
+  
+  let inputValue = document.getElementById('inputValue').value
+  let inner_box = document.querySelectorAll('.inner_ox_box')
+  
+  // check if inputvalue is used
+  // number hasn't been used
+  if( usedNumber.indexOf(inputValue) === -1){
+
+    // calculate current player
+    click = click + 1
+
+    // current player = playerOdd O
+    if( click%2 == 1){
+      inner_box[inputValue-1].innerHTML = 'O'
+      usedNumber.push(inputValue)
+      players.playerOdd.push(inputValue)
+      
+    }
+    // current player = playerEven X
+    else{
+      inner_box[inputValue-1].innerHTML = 'X'
+      usedNumber.push(inputValue)
+      players.playerEven.push(inputValue)
+    }
+  }
+  // number has been used
+  else{
+    alert("This number already used! Try another one.")
+  }
+})
