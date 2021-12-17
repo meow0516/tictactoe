c4fdc735c8f0cec086e7cd162bb440d40bc38413
AzureAD\JaneChan <meow0516@gmail.com>
2021-12-17 11:29:12 +0800

    main.js: import main.scss, update title

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-import './style.css'
+import './main.scss'
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <h1>This is a Tic Tac Toe!</h1>
 `
