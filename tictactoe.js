69f5721adc0efc4c0b8bc20b6d90a3b3b2e57d15
AzureAD\JaneChan <meow0516@gmail.com>
2021-12-20 15:31:25 +0800

    tictactoe.js: declare constructor

--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,30 @@
 class TicTacToe {
-  constructor() {}
+  constructor() {
+    this.isOddPlayerTurn = true;
+    this.usedNumber = [];
+    this.players = {
+      odd: {
+        name: 'Odd',
+        chosenNumber: [],
+        mark: 'O',
+      },
+      even: {
+        name: 'Even',
+        chosenNumber: [],
+        mark: 'X',
+      },
+    };
+    this.winnerArray = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+      [1, 5, 9],
+      [3, 5, 7],
+    ];
+  }
 }
 
 export default TicTacToe;
