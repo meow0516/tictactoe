93a7cedf7c24efd7bdf520e40371f284278a02c3
AzureAD\JaneChan <meow0516@gmail.com>
2021-12-20 11:43:37 +0800

    main.js: set prettier, simplify if-else check about valid input, used input and current player

--- main.js
+++ main.js
@@ -1,88 +1,65 @@
-import './main.scss'
-import '/node_modules/primeflex/primeflex.min.css'
-import '/node_modules/primeflex/themes/arya-blue.css'
+import './main.scss';
+import '/node_modules/primeflex/primeflex.min.css';
+import '/node_modules/primeflex/themes/arya-blue.css';
 
 let isOddPlayerTurn = true;
 let usedNumber = [];
 let players = {
   odd: {
-    name: "Odd",
-    chosenNumber: []
-  }, 
+    name: 'Odd',
+    chosenNumber: [],
+    mark: 'O',
+  },
   even: {
-    name: "Even",
-    chosenNumber: []
+    name: 'Even',
+    chosenNumber: [],
+    mark: 'X',
   },
 };
 let winnerArray = [
-  [1,2,3],
-  [4,5,6],
-  [7,8,9],
-  [1,4,7],
-  [2,5,8],
-  [3,6,9],
-  [1,5,9],
-  [3,6,7]
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
 ];
-  
 
+document.querySelector('.submit').addEventListener('click', function (e) {
+  let inputValue = document.getElementById('inputValue').valueAsNumber;
+  let innerBox = document.querySelectorAll('.inner_ox_box');
+  // check valid input
+  let isValidInput = inputValue > 0 && inputValue < 10;
+  if (!isValidInput) return alert('please input valid number');
 
-document.querySelector('.submit').addEventListener('click',function(e){
-  
-  let inputValue = document.getElementById('inputValue').valueAsNumber
-  let innerBox = document.querySelectorAll('.inner_ox_box')
-  if ( inputValue >0 && inputValue <10 ){
-    // check if inputvalue is used
-    // number hasn't been used
-    if( usedNumber.indexOf(inputValue) === -1){
+  // check input is used
+  let isUsedInput = usedNumber.indexOf(inputValue);
+  if (isUsedInput !== -1)
+    return alert('This number already used! Try another one.');
 
-      // current player = playerOdd O
-      if( isOddPlayerTurn ){
-        innerBox[inputValue-1].innerHTML = 'O'
-        usedNumber.push(inputValue)
-        players.odd.chosenNumber.push(inputValue)
-        
-        checkWinner(winnerArray, players.odd)
-      }
-      
-      // current player = playerEven X
-      else{
-        innerBox[inputValue-1].innerHTML = 'X'
-        usedNumber.push(inputValue)
-        players.even.chosenNumber.push(inputValue)
-        
-        checkWinner(winnerArray, players.even)
-      }
-      isOddPlayerTurn = !isOddPlayerTurn
-    }
-    // number has been used
-    else{
-      alert("This number already used! Try another one.")
-    }
+  // mark box
+  let currentPlayer = isOddPlayerTurn ? players.odd : players.even;
+  innerBox[inputValue - 1].innerHTML = currentPlayer.mark;
+  usedNumber.push(inputValue);
+  currentPlayer.chosenNumber.push(inputValue);
+  checkWinner(winnerArray, currentPlayer);
+  isOddPlayerTurn = !isOddPlayerTurn;
+});
 
-  }
-  else{
-    alert("please input valid number")
-  }
-  
-
-  
-})
-
-function checkWinner(winnerArray, playerInfo){
+function checkWinner(winnerArray, playerInfo) {
   for (const arr of winnerArray) {
-    if (arr.every(i => playerInfo.chosenNumber.includes(i))){
-    alert('Player ' + playerInfo.name + ' win!')
+    if (arr.every((i) => playerInfo.chosenNumber.includes(i))) {
+      alert('Player ' + playerInfo.name + ' win!');
     }
   }
 }
 
-// restart game 
-document.querySelector('.restart').addEventListener('click',function()
-{
-  let restartGame = confirm('Restart the game?')
-  if( restartGame ){
-    window.location.reload()
+// restart game
+document.querySelector('.restart').addEventListener('click', function () {
+  let restartGame = confirm('Restart the game?');
+  if (restartGame) {
+    window.location.reload();
   }
-})
-
+});
