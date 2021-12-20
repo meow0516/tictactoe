2e76eabf1dba88f4866c650d3cf2ec64da4a88db
AzureAD\JaneChan <meow0516@gmail.com>
2021-12-20 15:34:02 +0800

    tictactoe.js: add method submitValue, checkWinner

--- tictactoe.js
+++ tictactoe.js
@@ -33,6 +33,38 @@ class TicTacToe {
     });
   }
 
+  submitValue() {
+    let inputValue = document.getElementById('inputValue').valueAsNumber;
+    let innerBox = document.querySelectorAll('.inner_ox_box');
+
+    // check valid input
+    let isValidInput = inputValue > 0 && inputValue < 10;
+    if (!isValidInput) return alert('please input valid number');
+
+    // check input is used
+    let isUsedInput = this.usedNumber.includes(inputValue);
+    if (isUsedInput) return alert('This number already used! Try another one.');
+
+    // mark box
+    let currentPlayer = this.isOddPlayerTurn
+      ? this.players.odd
+      : this.players.even;
+    innerBox[inputValue - 1].innerHTML = currentPlayer.mark;
+    this.usedNumber.push(inputValue);
+    currentPlayer.chosenNumber.push(inputValue);
+    this.checkWinner(this.winnerArray, currentPlayer);
+
+    // players take turns
+    this.isOddPlayerTurn = !this.isOddPlayerTurn;
+  }
+
+  checkWinner(winnerArray, playerInfo) {
+    for (const arr of winnerArray) {
+      if (arr.every((value) => playerInfo.chosenNumber.includes(value))) {
+        alert('Player ' + playerInfo.name + ' win!');
+      }
+    }
+  }
 }
 
 export default TicTacToe;
