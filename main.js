da92e7225a0e4902807ffa21742a89c29462c0d9
AzureAD\JaneChan <meow0516@gmail.com>
2021-12-21 12:47:39 +0800

    main.js: new two tictactoe in the same page

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ import '/node_modules/primeflex/themes/arya-blue.css';
 import TicTacToe from './tictactoe.js';
 
 window.onload = function () {
-  let game1 = new TicTacToe();
+  let game1 = new TicTacToe('game1');
+  let game2 = new TicTacToe('game2');
   game1.gameInit();
+  game2.gameInit();
 };
 
 // restart game
